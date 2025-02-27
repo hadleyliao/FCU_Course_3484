a5d28b5e79be84c05a318b69ce66f8c1cf669a32
hadleyliao <hadleyliao@gmail.com>
2025-02-27 18:49:33 +0800

    解出1題

--- Homework_0227_1.java
+++ Homework_0227_1.java
@@ -3,15 +3,10 @@
 他所需投入各種幣值硬幣最少的數量分別是多少？若數字大於999則顯示：輸入過大。例如使用者輸入123，則程式顯示：10元12個，5元0個，1元3個。
 
 x<999
-x>999 => 提示報錯訊息
-123/10=12...3 => 12個10元 => 商 =>/
-                 0個5元
-                 3個1元   => 餘數 =>%
-
-
-128/10=12...8
-8/5=1...3
-3/1=3...0
+x>999 => 提示報錯
+123/10=12...3 => 商=12(10元) => 拿餘數往下除
+3/5=0...3     => 商=0(5元) => 拿餘數往下除
+3/1=3...0     => 商=3(1元)
 */
 
 
@@ -19,13 +14,16 @@ import java.util.Scanner;
 
 public class Homework_0227_1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        System.out.print("請輸入一個小於999的正整數：");
+        Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         if(x<999){
-            int result1 = x/10 ;
-            int result2 = x%10 ;
-
-            System.out.print("10元=" + "個" +"5元=" + "個" + "1元=" + "個");
+            int result1 = x/10 ;    // 只取商(10元有幾個)
+            int result2 = x%10 ;    // 取餘數往下運算
+            int result3 = x%10/5;   // 只取商(5元有幾個)
+            int result4 = x%10%5;   // 取餘數往下運算
+            int result5 = x%10%5/1; // 只取商(1元有幾個)
+            System.out.print("10元=" + result1 + "個" + "，5元=" +result3 + "個" + "，1元=" + result5 + "個");
         }else{
             System.out.print("輸入過大");
         }
