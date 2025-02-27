0286454eecadfbeec86985e008cbf98908d17aea
Hadley <hadleyliao@gmail.com>
2025-02-27 15:45:39 +0800

    寫上數學運算式

--- Homework_0227_2.java
+++ Homework_0227_2.java
@@ -1,11 +1,19 @@
 /*
 請撰寫一程式，計算出輸入之整數除以13的商數與餘數，
-例如：輸入26，輸出：傷數=2，餘數=0
+例如：輸入26，輸出：商數=2，餘數=0
+
+26/13=2...0
+
 */
 
 
+import java.util.Scanner;
+
 public class Homework_0227_2 {
     public static void main(String[] args) {
+        int n,a;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
 
     }
 }
--- Homework_0227_3.java
+++ Homework_0227_3.java
@@ -1,11 +1,33 @@
 /*
-雞兔同龍計算機程式，使用者輸入頭角分別是多少，
+雞兔同龍計算機程式，使用者輸入頭、腳分別是多少，
 程式輸出雞兔各有幾隻．例如：頭：8，腳26，輸出為：雞:3，兔:5，輸出：偶數
-*/
+head=x+y=8
+feet=2x+4y=26
 
+x=8-y
+2(8-y)+4y=26
+16-2y+4y=26
+2y=26-16
+2y=10
+y=5 帶入以下
+x=8-5=3
+x+y=8=偶數
+*/
+import java.util.Scanner;
 
 public class Homework_0227_3 {
     public static void main(String[] args) {
+        System.out.print("請輸入頭：");
+        Scanner sc1 = new Scanner(System.in);
+        int head = sc1.nextInt();
+
+        System.out.print("請輸入腳：");
+        Scanner sc2 = new Scanner(System.in);
+        int feet = sc2.nextInt();
+
+
+
+
 
     }
 }
