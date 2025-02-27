5f88cca528bf4244516383d18beed00af74ddf71
hadleyliao <hadleyliao@gmail.com>
2025-02-27 18:54:21 +0800

    去除投影片題目

--- _4_8_2_Conditional.java
+++
@@ -1,9 +0,0 @@
-public class _4_8_2_Conditional {
-    public static void main(String[] args) {
-        boolean lightIsOn = false;
-        System.out.println(lightIsOn? "燈亮了":"燈熄了"); //燈熄了
-
-        lightIsOn = !lightIsOn;
-        System.out.println(lightIsOn? "燈亮了":"燈熄了"); //燈亮了
-    }
-}
