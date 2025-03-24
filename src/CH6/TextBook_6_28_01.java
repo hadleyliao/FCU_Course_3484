8d2135feddb183ca2d7e8d138943f8f88c8e54f4
hadleyliao <hadleyliao@gmail.com>
2025-03-25 01:53:52 +0800

    新增

--- src/CH6/TextBook_6_28_01.java
+++ src/CH6/TextBook_6_28_01.java
@@ -7,7 +7,7 @@ package CH6;
 public class TextBook_6_28_01 {
     public static void main(String[] args) {
         int sum = 0, range = 100;
-        for(int i=3; i<=range; i+=3){ // i設定為3的倍數之最小起始值
+        for(int i = 3; i <= range; i += 3){ // i設定為3的倍數之最小起始值
             sum += i;
         }
         System.out.println("1 到 100 間所有 3 的倍數之總和為" + sum);
