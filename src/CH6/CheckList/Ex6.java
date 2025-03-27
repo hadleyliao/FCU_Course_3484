43ba05db54739af5771e85458320cc5cdbb3f87e
HadleyLiao <hadleyliao@gmail.com>
2025-03-27 10:59:15 +0800

    新增習題（？）

---
+++ src/CH6/CheckList/Ex6.java
@@ -0,0 +1,37 @@
+package CH6.CheckList;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        //(a) 輸出1、3、5、7、9
+//        for(int sum=0;sum<10;sum++) {
+//            sum = sum + 1;
+//            System.out.println(sum);
+//        }
+
+
+        //(b) 輸出抱錯 unreachable statement
+//        for(int sum=0; sum<10;sum++){
+//            sum = sum+1;
+//            break;
+//            System.out.println(sum);
+//        }
+
+
+        //(c) 輸出抱錯 unreachable statement
+//        int sum = 0;
+//        for(int i=0; i<10; i++){
+//            sum = sum + i;
+//            break;
+//            System.out.println(sum);
+//        }
+
+
+        //(d) 輸出抱錯 unreachable statement
+        int sum = 0;
+        for(int i=0;i<10;i++){
+            continue;
+            sum = sum + i;
+            System.out.println(sum);
+        }
+    }
+}
---
+++ src/CH6/CheckList/Ex7.java
@@ -0,0 +1,24 @@
+package CH6.CheckList;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        int X=10, Y=20;
+//        while (X<Y){
+//            X = X - Y;
+//            Y = Y - X;
+//            System.out.println("*");    // 輸出20個*
+//        }
+
+
+//        for(X=0; X<Y;X+=2){
+//            Y++;
+//            System.out.println("*");    // 輸出20個*
+//        }
+
+
+//        do{
+//            Y = Y-X;
+//            System.out.println("*");
+//        }while (X==Y)
+    }
+}
